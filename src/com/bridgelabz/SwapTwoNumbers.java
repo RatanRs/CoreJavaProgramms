fa8f08f0789ed76172e01dc949073b48bc9a9b73
9663461085 <ratansollapure16310@gmail.com>
2022-11-24 21:13:09 +0530

    SWappning Two numbers

---
+++ src/com/bridgelabz/SwapTwoNumbers.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class SwapTwoNumbers {
+	/***
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	
+int number1=10;
+int number2=20;
+System.out.println("before swlping numbers:"+number1+" "+number2);
+int swapNumber=number1;
+number1=number2;
+number2=swapNumber;
+System.out.println("after swaping numbers:" +number1+" "+number2);
+	}
+}
+
