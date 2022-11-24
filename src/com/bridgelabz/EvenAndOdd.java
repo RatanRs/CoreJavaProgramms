9b10c1ced0989a73559d771c29547cef2f65377c
9663461085 <ratansollapure16310@gmail.com>
2022-11-24 21:17:28 +0530

    Added:Checked the number is even or odd

---
+++ src/com/bridgelabz/EvenAndOdd.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class EvenAndOdd {
+	/***
+	 * 
+	 * @param args
+	 */
+public static void main(String[] args) {
+	
+	int number=1;
+	/*
+	 * Checking the condition by using if else condition
+	 */
+	if(number%2==0) {
+		System.out.println("Number is Even");
+	}
+	else {
+		System.out.println("Number is odd");
+	}
+}
+}
